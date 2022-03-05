d9adcee00f02d86195006d4d4d0e8a8be6332595
yuman211 <yukinuma0211@gmail.com>
2022-03-05 13:27:43 +0900

    ホーム画面作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\KakeiboController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//ホーム画面を表示
+Route::get('/', [KakeiboController::class, 'showHome'])->name('home');
