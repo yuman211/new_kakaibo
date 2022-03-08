035ad2b546dd561a8da3d0aabc74e30d96bf7cf0
yuman211 <yukinuma0211@gmail.com>
2022-03-09 07:15:39 +0900

    結果表示画面まで作成

--- resources/views/kakeibo/result.blade.php
+++ resources/views/kakeibo/result.blade.php
@@ -1,37 +1,38 @@
-<!-- @extends('layout')
+@extends('layout')
 
 @section('content')
 
-<div class="card my-2" style="width: 18rem;">
+<div class="card my-2">
     <div class="card-body">
         <h5 class="card-title1">総合計</h5>
-        <p class="card-text fs-4">{{number_format()}}円</p>
+        <p class="card-text fs-3">{{number_format($calculate_results['sum_all'])}}円</p>
         <h5 class="card-title2">一人当たり</h5>
-        <p class="card-text fs-4">{{number_format()}}円</p>
+        <p class="card-text fs-3">{{number_format($sum_each)}}円</p>
     </div>
 </div>
-<div class="card my-2" style="width: 18rem;">
+<div class="card my-2">
     <div class="card-body">
         <h5 class="card-title1">ゆうき</h5>
         <h5 class="card-title1">合計</h5>
-        <p class="card-text fs-4">{{number_format()}}円</p>
-        <h5 class="card-title2">差額</h5>
-        <p class="card-text fs-4">{{number_format()}}円</p>
+        <p class="card-text fs-3">{{number_format($calculate_results['sum_01'])}}円</p>
     </div>
 </div>
-<div class="card my-2" style="width: 18rem;">
+<div class="card my-2">
     <div class="card-body">
         <h5 class="card-title1">彩香</h5>
         <h5 class="card-title1">合計</h5>
-        <p class="card-text fs-4">{{number_format()}}円</p>
-        <h5 class="card-title2">差額</h5>
-        <p class="card-text fs-4">{{number_format()}}円</p>
+        <p class="card-text fs-3">{{number_format($calculate_results['sum_02'])}}円</p>
     </div>
 </div>
-
-<a href={{route('home')}}>
+<div class="card my-2">
+    <div class="card-body">
+        <h5 class="card-title1">差分</h5>
+        <p class="card-text fs-3">{{number_format($difference)}}円</p>
+    </div>
+</div>
+<a href={{route('create')}}>
     <div class="d-grid gap-2">
         <button type="button" class="btn btn-primary">OK</button>
     </div>
 </a>
-@endsection -->
+@endsection
